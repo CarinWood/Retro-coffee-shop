53ca43096111da1cdfcdd402aa6fdfa504ac2d9b
CarinWood <carin.wood.85@gmail.com>
2023-11-30 17:28:55 +0100

    first commit

---
+++ src/components/Hot.tsx
@@ -0,0 +1,12 @@
+import imageHot from '../assets/images/hot.jpg'
+
+export const Hot = () => {
+  return (
+    <div className="w-[100%} h-[100vh] flex justify-start items-start">
+        <img src={imageHot} 
+        alt="people in a coffee bar" 
+        className='w-[900px] h-[100vh]'
+        />
+    </div>
+  )
+}
